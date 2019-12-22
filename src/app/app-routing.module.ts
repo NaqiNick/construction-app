19935e6f31a31413c6715e0b22d0f7266bba9b45
NaqiNick <naqi.nick@gmail.com>
2019-12-22 22:46:27 +0500

    -- addition of public pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,20 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'public\landing', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'public\login',
+    loadChildren: () => import('./public/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'public\landing',
+    loadChildren: () => import('./public/landing/landing.module').then( m => m.LandingPageModule)
+  },
+  {
+    path: 'public\home',
+    loadChildren: () => import('./public/home/home.module').then( m => m.HomePageModule)
+  },
 ];
 
 @NgModule({
